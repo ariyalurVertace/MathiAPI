3f10d63923cacce897b24787c96cb45932b3e69b
malini <atttvrt021@gmail.com>
2021-11-24 18:28:22 +0530

    crud for sellerProfile,category

---
+++ src/controllers/custom/sellerprofile_controller.js
@@ -0,0 +1,170 @@
+import {genericResponse} from "../predefined/base_controller.js";
+import {
+    genericCreate,
+    genericGetAll,
+    genericGetOne,
+    genericUpdate,
+    genericDelete,
+    genericGetCount,
+} from "../predefined/generic_controller.js";
+import {statusCodes} from "../../config/constants.js";
+
+const Table = "sellerProfile";
+
+export async function create(req, res, next) {
+    try {
+        const json = req.body;
+        const item = await genericCreate({Table, json, req, res});
+        return genericResponse({
+            res,
+            result: item || null,
+            exception: null,
+            pagination: null,
+            statusCode: item ? statusCodes.SUCCESS : statusCodes.INVALID_DATA,
+        });
+    } catch (error) {
+        return next(error);
+    }
+}
+
+export async function update(req, res, next) {
+    try {
+        const json = req.body;
+        let newJson = {...json};
+        const item = await genericUpdate({
+            Table,
+            condition: {ID: parseInt(req.params.id, 10), IsDeleted: false},
+            json: newJson,
+            req,
+            res,
+        });
+        return genericResponse({
+            res,
+            result: null,
+            exception: null,
+            pagination: null,
+            statusCode: item ? statusCodes.SUCCESS : statusCodes.NOT_FOUND,
+        });
+    } catch (error) {
+        return next(error);
+    }
+}
+
+export async function remove(req, res, next) {
+    try {
+        let result = await genericDelete({
+            Table,
+            condition: {ID: parseInt(req.params.id, 10), IsDeleted: false},
+            softDelete: true,
+            req,
+            res,
+        });
+        return genericResponse({
+            res,
+            result: null,
+            exception: null,
+            pagination: null,
+            statusCode: result ? statusCodes.SUCCESS : statusCodes.NOT_FOUND,
+        });
+    } catch (error) {
+        return next(error);
+    }
+}
+
+export async function getOne(req, res, next) {
+    try {
+        let selectFields = {
+            id: true,
+            name: true,
+        };
+        let includeFields = null;
+        let sortConditions = null;
+
+        let item = await genericGetOne({
+            Table,
+            condition: {ID: parseInt(req.params.id, 10), IsDeleted: false},
+            selectFields,
+            includeFields,
+            sortConditions,
+        });
+
+        return genericResponse({
+            res,
+            result: item,
+            exception: null,
+            pagination: null,
+            statusCode: item ? statusCodes.SUCCESS : statusCodes.NOT_FOUND,
+        });
+    } catch (error) {
+        return next(error);
+    }
+}
+
+export async function getAll(req, res, next) {
+    try {
+        const json = req.body;
+        let condition = {IsDeleted: false};
+        let selectFields = {
+            ID: true,
+            Name: true,
+            Route: true,
+            Method: true,
+            APIModuleParameter: {
+                where: {
+                    IsDeleted: false,
+                },
+                select: {
+                    ID: true,
+                    Name: true,
+                    Variable: true,
+                    Location: true,
+                },
+            },
+        };
+        let includeFields = null;
+        let sortConditions = null;
+
+        const getResult = await genericGetAll({
+            Table,
+            condition,
+            selectFields,
+            includeFields,
+            sortConditions,
+            pageNumber: json.pageNumber,
+            pageLimit: json.pageLimit,
+        });
+        let {items, pagination} = getResult;
+
+        return genericResponse({
+            res,
+            result: items,
+            exception: null,
+            pagination,
+            statusCode: statusCodes.SUCCESS,
+        });
+    } catch (error) {
+        return next(error);
+    }
+}
+
+export async function getCount(req, res, next) {
+    try {
+        const json = req.body;
+        let condition = {IsDeleted: false};
+
+        let count = await genericGetCount({
+            Table,
+            condition,
+        });
+
+        return genericResponse({
+            res,
+            result: count,
+            exception: null,
+            pagination: null,
+            statusCode: statusCodes.SUCCESS,
+        });
+    } catch (error) {
+        return next(error);
+    }
+}
